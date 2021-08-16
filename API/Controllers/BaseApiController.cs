38f859bb8742337bad73fb6ace2f48319936cdb0
Reshma S <reshma97kaniyamparambil@gmail.com>
2021-08-16 14:48:32 +0530

    End of section 19

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,14 @@
+using API.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ServiceFilter(typeof(LogUserActivity))]
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class BaseApiController: ControllerBase
+    {
+        
+    }
+}
---
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -0,0 +1,35 @@
+import { NgForm } from '@angular/forms';
+import { MessageService } from './../../_services/message.service';
+import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Message } from 'src/app/_models/message';
+
+@Component({
+  changeDetection:ChangeDetectionStrategy.OnPush,
+  selector: 'app-member-messages',
+  templateUrl: './member-messages.component.html',
+  styleUrls: ['./member-messages.component.css']
+})
+export class MemberMessagesComponent implements OnInit {
+  @ViewChild('messageForm') messageForm:NgForm;
+  @Input() messages:Message[];
+  @Input() username:string;
+  messageContent : string;
+  loading = false;
+
+  constructor(public messageService : MessageService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  sendMessage(){
+    this.loading = true;
+    this.messageService.sendMessage(this.username, this.messageContent).then(() => {
+      this.messageForm.reset();
+    })
+    .finally(() => this.loading = false);
+   
+  }
+
+
+}
